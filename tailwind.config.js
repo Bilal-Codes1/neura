a353be63136c5ecb45f625039ec9329c1ff3b872
Bilal-Bytes <mb7899011@gmail.com>
2024-06-26 16:48:21 +0530

    COMMIT

--- tailwind.config.js
+++ tailwind.config.js
@@ -86,19 +86,3 @@ module.exports = {
   },
   plugins: [require("tailwindcss-animate")],
 };
-
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: [
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-
-    // Or if using `src` directory:
-    "./src/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-};
